2869c1a427c16da11fa991129eeca5c190ed1af4
mblondee <marjonblondeel@gmail.com>
2019-02-21 11:21:06 +0100

    directed dfs

---
+++ src/main/java/graphs/directed/DirectedDFS.java
@@ -0,0 +1,74 @@
+package graphs.directed;
+
+/*
+ * data type for finding paths from a source vertex to every other vertex in a
+ * simple directed and unweighted graph using depth first search
+ * to search in a graph: mark a vertex as visited and visit recursively all neighbouring vertices that have not yet been marked
+ * */
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class DirectedDFS<Vertex> {
+    private HashMap<Vertex, Boolean> markedVertices; // for every vertex v: is there a path from source to v?
+    private HashMap<Vertex, Vertex> edgeFrom; // for every vertex v: last vertex from source to v
+    private final Vertex source; // source vertex
+    private final DiGraph<Vertex> G;
+
+    public DirectedDFS(DiGraph<Vertex> G, Vertex source){
+        this.G = G;
+        this.source = source;
+        G.validate(source);
+        markedVertices = new HashMap<>();
+        edgeFrom = new HashMap<>();
+        //initialize hash maps
+        for(Vertex v : G.getVertices()){
+            markedVertices.put(v, false);
+            edgeFrom.put(v, null);
+        }
+        dfs(G, source);
+    }
+
+    // depth first search from vertex {@code v}
+    private void dfs(DiGraph<Vertex> G, Vertex v){
+        // mark v as visited
+        markedVertices.put(v, true);
+        // recursively visit all of v's neighbours that are not marked yet
+        for(Vertex w : G.getNeighbours(v)){
+            // if w is not marked
+            if(! markedVertices.get(w)){
+                edgeFrom.put(w, v);
+                dfs(G,w);
+            }
+        }
+    }
+
+    /*
+     * is there a directed path from source to vertex {@code w}?
+     * */
+    public boolean hasPathTo(Vertex w){
+        return marked(w);
+    }
+
+    // is {@code vertex} marked?
+    private boolean marked(Vertex w){
+        G.validate(w);
+        return markedVertices.get(w);
+    }
+
+    /*
+     * return path from source to vertex {@code w}
+     * */
+    public Iterable<Vertex> pathTo(Vertex w){
+        G.validate(w);
+        Stack<Vertex> path = new Stack<Vertex>() {
+        };
+        if(hasPathTo(w)){
+            for(Vertex v = w; v != source; v = edgeFrom.get(v)){
+                path.push(v);
+            }
+            path.push(source);
+        }
+        return path;
+    }
+}
---
+++ src/test/java/graphs/DirectedDFSTest.java
@@ -0,0 +1,65 @@
+package graphs;
+
+import graphs.directed.DiGraph;
+import graphs.directed.DirectedDFS;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.junit.Assert.*;
+
+public class DirectedDFSTest {
+
+    private static DiGraph<Integer> G;
+
+    @BeforeClass
+    public static void initialize(){
+        //String fileName = "src/main/resources/graph.txt";
+        String fileName = "src/main/resources/smalldigraph.txt";
+        G = new DiGraph();
+
+        String line;
+        try {
+            FileReader inputFile = new FileReader(fileName);
+            BufferedReader bufferReader = new BufferedReader(inputFile);
+            while ( (line = bufferReader.readLine()) != null){
+                String lineString = line.trim();
+                Integer v1 = Integer.parseInt(lineString.split("\\s+")[0]);
+                Integer v2 = Integer.parseInt(lineString.split("\\s+")[1]);
+                G.addVertex(v1);
+                G.addVertex(v2);
+                G.addEdge(v1,v2);
+            }
+            bufferReader.close();
+        }
+        catch(Exception e){
+            System.out.println("Error while reading file " + e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void test(){
+        DirectedDFS<Integer> dfs = new DirectedDFS<>(G,4 );
+        assertTrue(dfs.hasPathTo(2));
+        assertTrue(dfs.hasPathTo(3));
+        assertTrue(dfs.hasPathTo(0));
+        assertTrue(dfs.hasPathTo(5));
+        assertTrue(dfs.hasPathTo(1));
+        assertTrue(dfs.hasPathTo(4));
+
+        assertFalse(dfs.hasPathTo(6));
+        assertFalse(dfs.hasPathTo(7));
+        assertFalse(dfs.hasPathTo(8));
+        assertFalse(dfs.hasPathTo(9));
+        assertFalse(dfs.hasPathTo(10));
+        assertFalse(dfs.hasPathTo(11));
+        assertFalse(dfs.hasPathTo(12));
+
+        System.out.println(dfs.pathTo(5));
+
+
+    }
+}
