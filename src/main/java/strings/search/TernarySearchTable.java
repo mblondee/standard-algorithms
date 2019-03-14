b0af069adc4ede3e57ce61b26353e0657361240e
mblondee <marjonblondeel@gmail.com>
2019-03-14 10:45:48 +0100

    added prefix search, pattern matching

--- src/main/java/strings/search/TernarySearchTable.java
+++ src/main/java/strings/search/TernarySearchTable.java
@@ -3,7 +3,13 @@ package strings.search;
 /*
  * a symbol table of key-values pairs where the keys are strings
  * using ternary search trie
+ *
+ * return all keys with a given prefix
+ * return all keys that match a pattern (with wildcards)
+ * return longest prefix in set of keys of a given string
  * */
+
+import collections.queue.LinkedQueue;
 
 public class TernarySearchTable<Value> {
     private Node root; // root
@@ -45,7 +51,6 @@ public class TernarySearchTable<Value> {
         if(value == null){
             throw new IllegalArgumentException("value may not be null");
         }
-        // TODO: check if st already contains key
         if(! contains(key)){
             numberOfKeys++;
         }
@@ -138,5 +143,126 @@ public class TernarySearchTable<Value> {
             throw new IllegalArgumentException("key may not be null");
         }
         return get(key) != null;
+    }
+
+    /*
+     * return all keys as an Iterable
+     * */
+    public Iterable<String> keys(){
+        LinkedQueue<String> queue = new LinkedQueue<>();
+        collect(root, new StringBuilder(), queue);
+        return queue;
+    }
+
+    private void collect(Node node, StringBuilder string, LinkedQueue<String> queue ){
+        if(node == null){
+            return;
+        }
+        //left
+        collect(node.left, string, queue);
+
+        //mid
+        if(node.value != null){
+            // add string to collection
+            queue.enqueue(string.toString() + node.c);
+        }
+        collect(node.mid, string.append(node.c), queue);
+        string.deleteCharAt(string.length()-1);
+
+        //right
+        collect(node.right, string, queue);
+    }
+
+    public Iterable<String> keysWithPrefix(String prefix){
+        if(prefix == null){
+            throw new IllegalArgumentException("prefix cannot be null");
+        }
+        LinkedQueue<String>  queue = new LinkedQueue<>();
+        // find node that contains prefix
+        Node<Value> node = get(root, prefix, 0);
+        // check first node
+        if(node == null){
+            return queue;
+        }
+        if(node.value != null){
+            queue.enqueue(prefix);
+        }
+        // collect all keys that start from node
+        collect(node.mid, new StringBuilder(prefix), queue);
+        return queue;
+    }
+
+    /*
+     * return all keys that match {@code pattern} where . is a wildcard
+     * will look for strings with length of wildcard
+     * */
+    public Iterable<String> keysThatMatch(String pattern){
+        if(pattern == null){
+            throw new IllegalArgumentException("pattern cannot be null");
+        }
+        LinkedQueue<String>  queue = new LinkedQueue<>();
+        collect(root, new StringBuilder(), 0, pattern, queue);
+        return queue;
+    }
+
+    private void collect(Node node, StringBuilder stringToFit, int index, String pattern, LinkedQueue<String> queue){
+        if(node == null){
+            return;
+        }
+        char c = pattern.charAt(index);
+        if( c == '.' || c < node.c){
+            collect(node.left, stringToFit, index, pattern, queue);
+        }
+        if(c == '.' || c == node.c){
+            if(index == pattern.length() -1 && node.value != null){
+                queue.enqueue(stringToFit.toString() + node.c);
+            }
+            if( index < pattern.length()-1){
+                collect(node.mid, stringToFit.append(node.c), index+1, pattern, queue);
+                stringToFit.deleteCharAt(stringToFit.length()-1);
+            }
+        }
+        if(c == '.' || c > node.c){
+            collect(node.right, stringToFit, index, pattern, queue);
+        }
+    }
+
+    /*
+     * returns the string that is the longest prefix of {@code string}
+     * */
+    public String longestPrefixOf(String string){
+        if(string == null){
+            throw new IllegalArgumentException("input cannot be null");
+        }
+        // take care of empty string
+        if(string.length() == 0){
+            return null;
+        }
+        return longestPrefixOf(root, new StringBuilder(), string, 0, "");
+    }
+
+    private String longestPrefixOf(Node node, StringBuilder stringToFit, String string, int depth, String match){
+        if(node == null){
+            return match;
+        }
+        if(depth == string.length()){
+            return match;
+        }
+
+            char c = string.charAt(depth);
+            if(c < node.c){
+                return longestPrefixOf(node.left, stringToFit, string, depth, match);
+            }
+            else if(c > node.c){
+                return longestPrefixOf(node.right, stringToFit, string, depth, match);
+            }
+            else{
+                stringToFit.append(c);
+                if(node.value != null){
+                    match = stringToFit.toString();
+                }
+                return longestPrefixOf(node.mid, stringToFit, string, depth+1, match);
+            }
+
     }
 }
--- src/test/java/strings/TernarySearchTableTest.java
+++ src/test/java/strings/TernarySearchTableTest.java
@@ -1,5 +1,7 @@
 package strings;
 
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Assert;
 import org.junit.Test;
 import strings.search.TernarySearchTable;
 
@@ -24,6 +26,54 @@ public class TernarySearchTableTest {
         assertTrue(ST.contains("hallo"));
         assertFalse(ST.contains("halo"));
         assertTrue(ST.contains("haha"));
+    }
+
+    @Test
+    public void test1() {
+        String[] testArray = {"she", "sells", "seashells", "by", "the", "sea", "shore", "the", "shells", "she",
+                "sells", "are", "surely", "seashells"};
+        TernarySearchTable<Integer> ST = new TernarySearchTable<>();
+        for (String str : testArray) {
+            ST.put(str, 1);
+        }
+        Assert.assertThat(ST.keys(), IsIterableContainingInAnyOrder.containsInAnyOrder(new String[]{
+                "she", "sells", "seashells", "by", "the", "sea", "shore", "shells", "are", "surely"
+        }));
+        String[] testArrayPrefixSE = {"sells", "seashells", "sea"};
+        Assert.assertThat(ST.keysWithPrefix("se"), IsIterableContainingInAnyOrder.containsInAnyOrder(testArrayPrefixSE));
+
+        String[] testArrayPrefixS = {"she", "sells", "seashells", "sea", "shore", "surely", "shells"};
+        Assert.assertThat(ST.keysWithPrefix("s"), IsIterableContainingInAnyOrder.containsInAnyOrder(testArrayPrefixS));
+
+
+
+    }
+
+    @Test
+    public void test2(){
+        String[] testArray = {"ha", "haha", "hallo", "hahaha", "hello"};
+        TernarySearchTable<Integer> ST = new TernarySearchTable<>();
+        for(String str: testArray){
+            ST.put(str, 1);
+        }
+        Assert.assertThat(ST.keysThatMatch("h.llo"), IsIterableContainingInAnyOrder.containsInAnyOrder(
+                new String[]{"hallo", "hello"}
+        ));
+        assertEquals("hahaha", ST.longestPrefixOf("hahaha"));
+        assertEquals("haha", ST.longestPrefixOf("hahal"));
+        assertEquals("ha", ST.longestPrefixOf("hah"));
+        assertEquals("", ST.longestPrefixOf("aha"));
+        assertEquals("ha", ST.longestPrefixOf("hall"));
+        assertEquals("", ST.longestPrefixOf("hell"));
+
+        String[] test = {"your", "your mother", "your mother is", "your mother is in", "your mother is in the",
+                "your mother is in the kitchen"};
+        TernarySearchTable<Integer> ST1 = new TernarySearchTable<>();
+        for(String str: test){
+            ST1.put(str, 1);
+        }
+        assertEquals(6, ST1.size());
+        assertEquals("your mother is in the kitchen", ST1.longestPrefixOf("your mother is in the kitchen"));
     }
 
 }
