f693b666d2c314caaf18a0782686fa9cabfaa6c7
mblondee <marjonblondeel@gmail.com>
2019-03-22 11:24:21 +0100

    regexp

---
+++ src/main/java/strings/search/NFA.java
@@ -0,0 +1,163 @@
+package strings.search;
+
+/*
+* a data type for creating a non deterministic finite state automaton (NFA)
+* from a regular expression that tests whether a given string is matched by that regular expression
+*
+* a regular expression (RE) is
+* - empty: the empty set of strings
+* - a single character: set of strings with this 1 element (this one)
+* - a regular expression enclosed in parentheses
+* - two or more concatenated regular expressions: the cross product of the two sets of strings
+* - two or more regular expressions separated  by | (or): the union of the two sets of strings
+* - a regular expression followed by * (closure): any number of copies of the RE (also 0)
+*
+* assumption: all regexps are well defined, regexp may have wildcard '.' (can be any char)
+* string to test has no (,),*,|
+*
+* */
+
+import collections.bag.LinkedBag;
+import collections.stack.LinkedStack;
+import graphs.directed.DiGraph;
+import graphs.directed.DirectedDFS;
+import graphs.directed.DirectedEdge;
+
+public class NFA {
+    private DiGraph<Integer> graph; // digraph of epsilon (no match) transitions
+    private String regexp; // regular expression
+    private int reglength; // length of regular expression
+
+    public NFA(String regexp){
+        this.regexp = regexp;
+        this.reglength = regexp.length();
+
+        // stack to keep track of parentheses and |
+        // each left parenthesis is pushed on the stack, when a right parenthesis is encountered
+        // the corresponding left one or | is popped
+        LinkedStack<Integer> operators = new LinkedStack<>();
+
+        graph = new DiGraph<>();
+        // the nfa has a state for each character in the regexp + an accept state
+        for(Integer i = 0; i<=reglength; i++){
+          graph.addVertex(i);
+        }
+
+        for(Integer i = 0; i<reglength; i++){
+            int leftParenthesis = i; //left parenthesis or current state
+
+            if(regexp.charAt(i) == '(' || regexp.charAt(i) == '|'){
+                operators.push(i);
+            }
+
+            else if(regexp.charAt(i) == ')'){
+                int op = operators.pop();
+
+                //check if op is ) or |
+                if(regexp.charAt(op) == '|'){
+                    // get state of left parenthesis
+                    leftParenthesis = operators.pop();
+                    // add edge from left parenthesis to state after | (choice of second operand)
+                    graph.addEdge(new DirectedEdge<>(leftParenthesis, op+1));
+                    // add edge from | to right parenthesis (choice of first operand)
+                    graph.addEdge(new DirectedEdge<>(op, i));
+                }
+
+                else if(regexp.charAt(op) == '('){
+                    leftParenthesis = op;
+                }
+
+            }
+
+            // is next char *?
+            if(i < reglength -1 && regexp.charAt(i+1) == '*'){
+                graph.addEdge(new DirectedEdge<>(leftParenthesis, i+1));
+                graph.addEdge(new DirectedEdge<>(i+1, leftParenthesis));
+            }
+
+            // add epsilon transitions
+            if(regexp.charAt(i) == '(' || regexp.charAt(i) == '*' || regexp.charAt(i) == ')'){
+                graph.addEdge(new DirectedEdge<>(i, i+1));
+            }
+
+        }
+
+
+    }
+
+    /*
+    * is text matched by regexp?
+    * */
+    public boolean matches(String text){
+        // searching path from state 0
+        DirectedDFS<Integer> dfs = new DirectedDFS<>(graph, 0);
+
+        LinkedBag<Integer> reachableStates = new LinkedBag<>();
+
+        // add all states reachable from state 0 with epsilon transitions (nfa)
+        for(int i = 0; i <= reglength; i++){
+            if(dfs.hasPathTo(i)){
+                reachableStates.add(i);
+            }
+        }
+
+        // going through text
+        for(int i = 0; i < text.length(); i++){
+            LinkedBag<Integer> matches = new LinkedBag<>();
+
+            for(int state : reachableStates){
+                // add end of regexp
+                if(state == reglength){
+                    continue;
+                }
+                // char is one of the states: add next state
+                // or regexp has wildcard
+                if( regexp.charAt(state) == text.charAt(i) || regexp.charAt(state) == '.' ){
+                    matches.add(state +1);
+                }
+            }
+
+            //reset reachable states
+            reachableStates = new LinkedBag<>();
+
+            // add states reachable by epsilon transitions from the states in matches
+            for(int state : matches){
+                dfs = new DirectedDFS<>(graph, state);
+                // add all states reachable from state with epsilon transitions (nfa)
+                for(int otherState = 0; otherState <= reglength; otherState++){
+                    if(dfs.hasPathTo(otherState)){
+                        reachableStates.add(otherState);
+                    }
+                }
+            }
+
+            // there are no reachable states
+            if(reachableStates.size() == 0){
+                return false;
+            }
+
+        }
+
+        // when gone through text, check for accept state
+        for(int state : reachableStates){
+            if(state == reglength){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return graph.toString();
+    }
+
+    public DiGraph<Integer> getGraph(){
+        return graph;
+    }
+
+
+}
---
+++ src/test/java/strings/RegExpTest.java
@@ -0,0 +1,103 @@
+package strings;
+
+import graphs.directed.DirectedEdge;
+import org.junit.Assert;
+import org.junit.Test;
+import strings.search.NFA;
+
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+
+import static org.junit.Assert.*;
+
+public class RegExpTest {
+
+    @Test
+    public void testnfa1(){
+        NFA nfa = new NFA("(AB)*");
+        assertEquals(6, nfa.getGraph().numberOfVertices());
+
+        DirectedEdge<Integer>[] a = new DirectedEdge[5];
+        a[0] = new DirectedEdge<>(0,1);
+        a[1] = new DirectedEdge<>(0,4);
+        a[2] = new DirectedEdge<>(3,4);
+        a[3] = new DirectedEdge<>(4,0);
+        a[4] = new DirectedEdge<>(4,5);
+
+
+        Assert.assertThat(nfa.getGraph().getEdges(), IsIterableContainingInAnyOrder.containsInAnyOrder(a
+        ));
+
+       assertTrue(nfa.matches("AB"));
+        assertTrue(nfa.matches(""));
+        assertTrue(nfa.matches("ABABAB"));
+        assertFalse(nfa.matches("BB"));
+        assertFalse(nfa.matches("BA"));
+        assertFalse(nfa.matches("C"));
+    }
+
+    @Test
+    public void testnfa2(){
+        NFA nfa = new NFA("(A|B)*");
+        assertEquals(7, nfa.getGraph().numberOfVertices());
+
+        DirectedEdge<Integer>[] a = new DirectedEdge[7];
+        a[0] = new DirectedEdge<>(0,1);
+        a[1] = new DirectedEdge<>(0,3);
+        a[2] = new DirectedEdge<>(0,5);
+        a[3] = new DirectedEdge<>(2,4);
+        a[4] = new DirectedEdge<>(4,5);
+        a[5] = new DirectedEdge<>(5,0);
+        a[6] = new DirectedEdge<>(5,6);
+
+
+        Assert.assertThat(nfa.getGraph().getEdges(), IsIterableContainingInAnyOrder.containsInAnyOrder(a
+        ));
+
+        assertTrue(nfa.matches("A"));
+        assertTrue(nfa.matches("AA"));
+        assertTrue(nfa.matches("AB"));
+        assertTrue(nfa.matches(""));
+        assertTrue(nfa.matches("BB"));
+        assertFalse(nfa.matches("BC"));
+        assertFalse(nfa.matches("CA"));
+    }
+
+    @Test
+    public void testnfa3(){
+        NFA nfa = new NFA("((A*B|AC)D)");
+        assertEquals(12, nfa.getGraph().numberOfVertices());
+
+        DirectedEdge<Integer>[] a = new DirectedEdge[9];
+        a[0] = new DirectedEdge<>(0,1);
+        a[1] = new DirectedEdge<>(1,2);
+        a[2] = new DirectedEdge<>(1,6);
+        a[3] = new DirectedEdge<>(2,3);
+        a[4] = new DirectedEdge<>(3,2);
+        a[5] = new DirectedEdge<>(3,4);
+        a[6] = new DirectedEdge<>(5,8);
+        a[7] = new DirectedEdge<>(8,9);
+        a[8] = new DirectedEdge<>(10,11);
+
+
+        Assert.assertThat(nfa.getGraph().getEdges(), IsIterableContainingInAnyOrder.containsInAnyOrder(a
+        ));
+
+        assertTrue(nfa.matches("AABD"));
+        assertTrue(nfa.matches("AAAAAAAAAAAAAAAAAAAABD"));
+        assertFalse(nfa.matches("AAAAAAAAAAAAAAAAAAAAD"));
+
+        assertTrue(nfa.matches("ACD"));
+        assertFalse(nfa.matches("AACD"));
+    }
+
+    @Test
+    public void wildcardTest(){
+        NFA nfa = new NFA("(A.B)*");
+        assertTrue(nfa.matches("AAB"));
+        assertTrue(nfa.matches("ACB"));
+        assertTrue(nfa.matches("AABACBABB"));
+        assertFalse(nfa.matches("AB"));
+        assertFalse(nfa.matches("BCA"));
+    }
+
+}
